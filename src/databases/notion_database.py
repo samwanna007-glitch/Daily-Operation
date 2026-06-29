29c8526a9e47d14ca62e96566dbd78d2b093b6a3
unknown <noahcd7771@gmail.com>
2026-06-29 18:20:55 +0700

    add feather weather tracker

--- src/databases/notion_database.py
+++ src/databases/notion_database.py
@@ -1,10 +1,11 @@
 import requests
 
+
 class NotionDatabase:
     def __init__(self, api_key, database_id):
-        if not api_key or not database_id: 
+        if not api_key or not database_id:
             raise ConnectionError("Notion Database: Require api key AND database id")
-            
+
         self.api_key = api_key
         self.database_id = database_id
         self.headers = {
@@ -14,40 +15,44 @@ class NotionDatabase:
         }
         self.base_url = "https://api.notion.com/v1"
 
-        print("Initialized notion database.")
-    
+        print("Notion init.")
+
     def check_property_value(self, property_name, target_value, property_type):
         url = f"{self.base_url}/databases/{self.database_id}/query"
 
         if property_type not in ["number", "title", "rich_text", "url"]:
-            raise ValueError("property_type must be 'number', 'title', 'rich_text', or 'url'.")
-            
+            raise ValueError(
+                "property_type must be 'number', 'title', 'rich_text', or 'url'."
+            )
+
         filter_payload = {
             "property": property_name,
-            property_type: {"equals": target_value}
+            property_type: {"equals": target_value},
         }
-        
+
         payload = {"filter": filter_payload}
 
         try:
             response = requests.post(url, headers=self.headers, json=payload)
             data = response.json()
             results = data.get("results")
-            return len(results) > 0                
+            return len(results) > 0
         except Exception as e:
             raise ValueError(f"Network Connection Error during Notion query: {e}")
-    
+
     def add_row(self, properties):
         url = f"{self.base_url}/pages"
         payload = {
             "parent": {"database_id": self.database_id},
-            "properties": properties
+            "properties": properties,
         }
 
         try:
-            response = requests.post(url, headers=self.headers, json=payload, timeout=10)
-            response.raise_for_status() 
-            return "Successfully added row."
-            
+            response = requests.post(
+                url, headers=self.headers, json=payload, timeout=10
+            )
+            response.raise_for_status()
+            return "Successfully added item to notion database."
+
         except Exception as e:
             return f"Failed to add row. Error: {e}"
---
+++ src/main_weather.py
@@ -0,0 +1,158 @@
+import sys
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+import requests
+
+from config import (
+    NOTION_API_KEY,
+    NOTION_WEATHER_DATABASE_ID,
+    OPENWEATHERMAP_API_KEY,
+    OPENWEATHERMAP_CITY,
+)
+from databases import NotionDatabase
+
+
+def check_config_variables():
+    if OPENWEATHERMAP_API_KEY is None:
+        print("Missing API key!")
+        sys.exit()
+
+    if NOTION_API_KEY is None:
+        print("Missing Database API KEY!")
+        sys.exit()
+
+    if NOTION_WEATHER_DATABASE_ID is None:
+        print("Missing Database URL!")
+        sys.exit()
+
+    print("Start process.")
+
+
+def get_coord(city: str):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHERMAP_API_KEY}&units=metric"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return {
+            "lon": data.get("coord").get("lon"),
+            "lat": data.get("coord").get("lat"),
+            "country": data.get("sys").get("country"),
+            "name": data.get("sys").get("name"),
+        }
+
+    except requests.exceptions.RequestException as e:
+        print(f"Network or API Error: {e}")
+        return None
+
+
+def fetch_24_hour_forecast(lat, lon, api_key):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&units=metric&appid={api_key}"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        next_24_hours = data["list"][:8]
+
+        simplified_forecast = []
+        for item in next_24_hours:
+            forecast_point = {
+                "date": item["dt_txt"],
+                "temp": item["main"]["temp"],
+                "weather": item["weather"][0]["main"],
+                "description": item["weather"][0]["description"],
+            }
+            simplified_forecast.append(forecast_point)
+        return simplified_forecast
+
+    except Exception as e:
+        print(f"Error fetching the 24-hour forecast: {e}")
+
+
+# support only 10 countries
+def convert_timezone(utc_string: str, target_tz: str) -> str:
+    target_tz = target_tz.lower().strip()
+    match target_tz:
+        case "thailand" | "th":
+            simplified_target_tz = "Asia/Bangkok"
+        case "united states" | "us" | "usa":
+            simplified_target_tz = "America/New_York"
+        case "united kingdom" | "uk":
+            simplified_target_tz = "Europe/London"
+        case "japan" | "jp":
+            simplified_target_tz = "Asia/Tokyo"
+        case "china" | "cn":
+            simplified_target_tz = "Asia/Shanghai"
+        case "india" | "in":
+            simplified_target_tz = "Asia/Kolkata"
+        case "germany" | "de":
+            simplified_target_tz = "Europe/Berlin"
+        case "france" | "fr":
+            simplified_target_tz = "Europe/Paris"
+        case "italy" | "it":
+            simplified_target_tz = "Europe/Rome"
+        case "canada" | "ca":
+            simplified_target_tz = "America/Toronto"
+        case _:
+            simplified_target_tz = "UTC"
+
+    try:
+        utc_time = datetime.strptime(utc_string, "%Y-%m-%d %H:%M:%S").replace(
+            tzinfo=ZoneInfo("UTC")
+        )
+
+        thailand_time = utc_time.astimezone(ZoneInfo(simplified_target_tz))
+
+        return thailand_time.strftime("%Y-%m-%d %H:%M:%S")
+    except Exception as e:
+        print(f"Error converting time: {e}")
+        return utc_string
+
+
+if __name__ == "__main__":
+    check_config_variables()
+
+    # setup notion
+    notion = NotionDatabase(
+        api_key=NOTION_API_KEY, database_id=NOTION_WEATHER_DATABASE_ID
+    )
+
+    # get coord
+    coord = get_coord(city=OPENWEATHERMAP_CITY)
+    if coord is not None:
+        lon = coord.get("lon")
+        lat = coord.get("lat")
+
+        forecast_data_24_hour = fetch_24_hour_forecast(
+            lon=lon, lat=lat, api_key=OPENWEATHERMAP_API_KEY
+        )
+
+        # get forecast in the next 24 hours
+        if forecast_data_24_hour is not None:
+            for w in forecast_data_24_hour:
+                properties = {
+                    "title": {"title": [{"text": {"content": "Weather report"}}]},
+                    "date": {
+                        "rich_text": [
+                            {
+                                "text": {
+                                    "content": convert_timezone(
+                                        utc_string=w.get("date"),
+                                        target_tz=OPENWEATHERMAP_CITY,
+                                    )
+                                }
+                            }
+                        ]
+                    },
+                    "temp": {"number": float(w.get("temp"))},
+                    "weather": {"rich_text": [{"text": {"content": w.get("weather")}}]},
+                    "description": {
+                        "rich_text": [{"text": {"content": w.get("description")}}]
+                    },
+                    "country": {"select": {"name": OPENWEATHERMAP_CITY}},
+                }
+
+                # add to notion database
+                response = notion.add_row(properties=properties)
+                print(response)
